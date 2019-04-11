edd92b375712a89c1199c1116b3149dd6e1c3957
jamadom07683 <jamadom07683@universidadean.edu.co>
2019-04-11 13:52:49 -0500

    Php

--- Clases/prueba.php
+++ Clases/prueba.php
@@ -6,6 +6,8 @@
     <meta name=“viewport” content=“width=device-width”>
 </head>
 <body>
+
+  <h1><?php echo 'Inicio sesion'; ?></h1>
   <h1>Este es el ejemplo de php basico</h1>
   <form action=“test.php” method=“get”>
   <input type=“text” placeholder=“usuario” name=“txtusuario” />
